d790d43d879e081516669ed99bf4ed60b852f01a
Jack Parsons <jackpar1113@gmail.com>
2026-01-17 15:42:33 -0700

    models!

--- src/main.zig
+++ src/main.zig
@@ -1,17 +1,17 @@
 const std = @import("std");
 const runtime = @import("zephyr_runtime");
-const obj = runtime.obj;
+const RenderCommand = runtime.RenderCommand;
 const Input = runtime.Input;
+const obj = runtime.obj;
 
 pub const std_options = runtime.recommended_std_options;
 
 const movement_speed = 0.2;
 
 const GameScene = struct {
-    vao: runtime.VertexArray,
+    model: runtime.Model,
     shader: runtime.Shader,
     material: runtime.Material,
-    transparency: f32,
     camera: runtime.Camera,
 
     pub fn create(allocator: std.mem.Allocator, props: runtime.ApplicationProps) !*GameScene {
@@ -27,10 +27,9 @@ const GameScene = struct {
         );
 
         self.* = GameScene{
-            .vao = undefined,
+            .model = undefined,
             .shader = undefined,
             .material = undefined,
-            .transparency = 0.0,
             .camera = camera,
         };
         return self;
@@ -39,45 +38,27 @@ const GameScene = struct {
     pub fn onStartup(self: *GameScene, allocator: std.mem.Allocator) !void {
         std.log.info("GameScene starting up...", .{});
 
-        var mesh = try obj.parse(allocator, @embedFile("assets/meshes/monkey.obj"));
-        defer mesh.deinit();
-
-        self.vao = runtime.VertexArray.init(mesh.vertices, mesh.indices);
-
         const vs_src = @embedFile("assets/shaders/vertex.glsl");
         const fs_src = @embedFile("assets/shaders/fragment.glsl");
+        const obj_src = @embedFile("assets/meshes/monkey.obj");
         self.shader = try runtime.Shader.init(allocator, vs_src, fs_src);
         self.material = try runtime.Material.init(allocator, &self.shader);
-
-        self.vao.setLayout(self.shader.buffer_layout);
+        self.model = try runtime.Model.init(allocator, obj_src, &self.material, .zero);
     }
 
     pub fn onUpdate(self: *GameScene, delta_time: f32) void {
-        runtime.RenderCommand.Clear(.{ .x = 0.4, .y = 0.4, .z = 0.4 });
+        RenderCommand.Clear(.{ .x = 0.4, .y = 0.4, .z = 0.4 });
         const speed = movement_speed * delta_time;
 
-        if (Input.IsKeyPressed(.Escape)) {
-            std.log.info("Escape key pressed, exiting...", .{});
-        } else if (Input.IsKeyHeld(.Space)) {
-            std.log.info("Space key pressed!", .{});
-        } else if (Input.IsKeyHeld(.A)) {
-            self.transparency += speed;
-            if (self.transparency > 1.0) {
-                self.transparency = 1.0;
-            }
-            // self.position.x -= speed;
+        if (Input.IsKeyHeld(.A)) {
+            self.model.position.x -= speed;
         } else if (Input.IsKeyHeld(.D)) {
-            self.transparency -= speed;
-            if (self.transparency < 0.0) {
-                self.transparency = 0.0;
-            }
-            // self.position.x += speed;
+            self.model.position.x += speed;
+        } else if (Input.IsKeyHeld(.W)) {
+            self.model.position.z += speed;
+        } else if (Input.IsKeyHeld(.S)) {
+            self.model.position.z -= speed;
         }
-        // else if (Input.isKeyHeld(.W)) {
-        //     self.position.y += speed;
-        // } else if (Input.isKeyHeld(.S)) {
-        //     self.position.y -= speed;
-        // }
 
         if (Input.IsButtonHeld(.Left)) {
             const delta = Input.GetMouseMoveDelta();
@@ -92,10 +73,7 @@ const GameScene = struct {
             self.camera.zoom(delta.y, speed);
         }
 
-        // self.material.setUniform("r_color", self.transparency);
-        self.material.setUniform("r_position", self.camera.viewProjectionMatrix());
-
-        runtime.RenderCommand.Draw(self.vao);
+        RenderCommand.Draw(&self.model, &self.camera);
     }
 
     pub fn onEvent(self: *GameScene, e: runtime.ZEvent) void {
